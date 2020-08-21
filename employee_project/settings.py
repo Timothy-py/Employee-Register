07c3b352deecdbd0f9290368b0d43ab617b67c7f
Timothy-py <adeyeyetimothy33@gmail.com>
2020-08-21 23:58:15 +0100

    added db configurations :hammer:

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'employeedb',
+        'USER': 'timothy',
+        'PASSWORD': 'plati442',
+        'HOST': 'localhost'
     }
 }
 
